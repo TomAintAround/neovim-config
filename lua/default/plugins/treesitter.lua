df0c002cf6008e69147878765fa22ff0e51397a2
TomAintAround <tcorreia2005@proton.me>
2025-03-30 18:25:13 +0100

    chore: make the treesitter plugin file diagnostics shut up

--- lua/default/plugins/treesitter.lua
+++ lua/default/plugins/treesitter.lua
@@ -5,11 +5,16 @@ return {{
 	config = function()
 		require("nvim-treesitter.configs").setup {
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			sync_install = false,
 			auto_install = true,
+			ignore_install = {},
+			modules = {},
 			highlight = {
 				enable = true,
+				---@diagnostic disable-next-line
 				disable = function(lang, buf)
 					local max_filesize = 100 * 1024 -- 100 KB
+					---@diagnostic disable-next-line
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
 						return true
