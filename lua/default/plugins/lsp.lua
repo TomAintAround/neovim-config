04b922bdd956b3165fd7841bcea0384dc1a1f8af
TomAintAround <tcorreia2005@proton.me>
2025-03-31 00:32:43 +0100

    feat: format on every save

--- lua/default/plugins/lsp.lua
+++ lua/default/plugins/lsp.lua
@@ -97,6 +97,23 @@ return {{
 			end
 		end
 
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client then return end
+
+				---@diagnostic disable-next-line
+				if client.supports_method("textDocument/formatting", 0) then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+						end,
+					})
+				end
+			end
+		})
+
 		require("fidget").setup({})
 	end,
 }}
