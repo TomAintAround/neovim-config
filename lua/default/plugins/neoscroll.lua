db4769c914c83d20e81ce83ea1b53059491d7c6e
TomAintAround <tcorreia2005@proton.me>
2025-03-31 03:28:23 +0100

    style: use opts instead of require(...).setup in some scenarios

--- lua/default/plugins/neoscroll.lua
+++ lua/default/plugins/neoscroll.lua
@@ -1,9 +1,7 @@
 return { {
 	"karb94/neoscroll.nvim",
 	enabled = true,
-	config = function()
-		require("neoscroll").setup({
-			duration_multiplier = 0.5,
-		})
-	end
+	opts = {
+		duration_multiplier = 0.5,
+	},
 } }
--- lua/default/plugins/treesitter.lua
+++ lua/default/plugins/treesitter.lua
@@ -3,29 +3,27 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		enabled = true,
 		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-				sync_install = false,
-				auto_install = true,
-				ignore_install = {},
-				modules = {},
-				highlight = {
-					enable = true,
+		opts = {
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			sync_install = false,
+			auto_install = true,
+			ignore_install = {},
+			modules = {},
+			highlight = {
+				enable = true,
+				---@diagnostic disable-next-line
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
 					---@diagnostic disable-next-line
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						---@diagnostic disable-next-line
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-						return false
-					end,
-					additional_vim_regex_highlighting = true,
-				},
-			})
-		end,
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+					return false
+				end,
+				additional_vim_regex_highlighting = true,
+			},
+		},
 	},
 	{
 		"tree-sitter-grammars/tree-sitter-hyprlang",
