0c2460ab1f0be8a7bed6d0d7228f174d984a0516
TomAintAround <tcorreia2005@proton.me>
2025-03-31 22:20:39 +0100

    feat: add noice plugin

---
+++ lua/default/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"folke/noice.nvim",
+		enabled = true,
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		opts = {},
+	},
+}
