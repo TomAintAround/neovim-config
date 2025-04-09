a8518807b324710aa8619ccb15dc572a9408774b
TomAintAround <tcorreia2005@proton.me>
2025-04-09 22:19:43 +0100

    fix: show macro recording notifications again

--- lua/default/plugins/noice.lua
+++ lua/default/plugins/noice.lua
@@ -7,6 +7,31 @@ return {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
 		},
-		opts = {},
+		config = function()
+			---@diagnostic disable-next-line
+			require("noice").setup({
+				routes = {
+					{
+						view = "notify",
+						filter = { event = "msg_showmode" },
+					},
+					{
+						view = "mini",
+						filter = {
+							event = "msg_showcmd",
+							any = {
+								{ find = "[mq]$" },
+							},
+						},
+					},
+				},
+			})
+			vim.api.nvim_create_autocmd("RecordingLeave", {
+				callback = function()
+					-- Display a message when macro recording stops
+					print("Macro recording stopped")
+				end,
+			})
+		end,
 	},
 }
