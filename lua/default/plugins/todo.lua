8ea4f6ac2c632c011c4f3e4deae73443184511de
TomAintAround <tcorreia2005@proton.me>
2025-04-14 20:39:18 +0100

    feat: add todo-comments plugin

---
+++ lua/default/plugins/todo.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"folke/todo-comments.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("todo-comments").setup()
+			vim.keymap.set("n", "<leader>t", vim.cmd.TodoTelescope)
+		end,
+	},
+}
