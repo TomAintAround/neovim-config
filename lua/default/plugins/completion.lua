97675f09dfeacf5385bd32980d499da12dd9b0be
TomAintAround <tcorreia2005@proton.me>
2025-10-17 19:15:41 +0100

    feat(completion): updated config

--- lua/default/plugins/completion.lua
+++ lua/default/plugins/completion.lua
@@ -5,14 +5,15 @@ return {
 		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "1.*",
 		opts = {
-			keymap = { preset = "default" },
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
+			keymap = {
+				["<C-u>"] = { "scroll_documentation_up", "fallback" },
+				["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			},
+			completion = {
+				documentation = { auto_show = true },
+				ghost_text = { enabled = true },
 			},
 			signature = { enabled = true },
-			completion = { documentation = { auto_show = false } },
-			fuzzy = { implementation = "prefer_rust" },
 		},
 		opts_extend = { "sources.default" },
 	},
